3a494732ebb175fb1f81b1505d797554ff6b5fe2
Rochelle Taylor <rochelletaylor614@gmail.com>
2018-03-15 10:13:07 -0400

    initial commit

---
+++ src/main/java/org/wcci/citiesstates/CitiesStatesApplication.java
@@ -0,0 +1,12 @@
+package org.wcci.citiesstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesStatesApplication.class, args);
+	}
+}
